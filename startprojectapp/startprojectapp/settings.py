20699413ade7c8bac19b7cbfd5b95b5f698772d6
Sergio Arellanom <sergioarellanodiaz@gmail.com>
2023-03-01 11:51:23 +0100

    implements externalize configuration data in Python, in file setting.ini

--- startprojectapp/startprojectapp/settings.py
+++ startprojectapp/startprojectapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w^j2gt8n4^!glb64w-0so55n&@ygc)#(2_zl-yb2pl4b%cc^=7'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -134,7 +135,7 @@ LOGGING = {
         'file': {
             'level': 'ERROR',
             'class': 'logging.FileHandler',
-            'filename': BASE_DIR / 'log/django-start-project.log',
+            'filename': BASE_DIR / 'log/' / config('NAME_FILE_LOG'),
         },
     },
     'loggers': {
